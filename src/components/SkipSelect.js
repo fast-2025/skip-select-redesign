538bdc2e799cc2e79022dcfa8438801bb53a4d24
fast-2025 <samiahonen1108@gmail.com>
2025-04-02 02:18:44 +0300

    Added SkipSelect component

---
+++ src/components/SkipSelect.js
@@ -0,0 +1,11 @@
+import React, {useEffect, useState } from "react";
+import axios from "axios";
+
+function SkipSelect() {
+    
+    return (
+        <div>Hi</div>
+    )
+}
+
+export default SkipSelect;
